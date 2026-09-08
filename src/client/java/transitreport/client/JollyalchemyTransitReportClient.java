1948547d5397499de158c78b202ca9e2bf0671f2
Nate Neibauer <nneibauer044@gmail.com>
2026-09-07 21:35:07 -0700

    Initial template from Fabric

---
+++ src/client/java/transitreport/client/JollyalchemyTransitReportClient.java
@@ -0,0 +1,10 @@
+package transitreport.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JollyalchemyTransitReportClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/transitreport/client/JollyalchemyTransitReportDataGenerator.java
@@ -0,0 +1,11 @@
+package transitreport.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JollyalchemyTransitReportDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
